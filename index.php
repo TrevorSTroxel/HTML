8ede1eb0d6ee31db4c6605e647bcbbe3b9f3748f
TrevorSTroxel <actroxel@gmail.com>
2016-10-25 12:11:44 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Delta</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--  There are no external links to css or js-->
+  <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+   <link rel="stylesheet" href="dist/css/custom.css">
+  <script src="dist/jquery/jquery.min.js"></script>
+  <script src="dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+<!--  add a menu here -->
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Delta</a>
+    </div>
+    
+    <ul class="nav navbar-nav">
+		<li class="active"><img src ="img/delta.png" height = "50"></li>
+      <li><a href="http://10.183.2.4">Home</a></li>
+      <li><a href="download/"target = "_blank">Downloads</a></li>
+      <li><a href="javascript/"target = "_blank">Java Script</a></li>
+      <li><a href="phpcode/"target = "_blank">PHP Code</a></li>
+      <li><a href="pma/"target = "_blank">PMA</a></li>
+      <li><a href="contact.html">Contact</a></li>
+    </ul>
+        <div class="btn-group">
+  <button type="button" class="btn btn-default disabled btn-md">Click bait</button></a>
+  <button type="button" class="btn btn-primary disabled btn-md">More click bait</button>
+  <button type="button" class="btn btn-success disabled btn-md">Even more click bait</button>
+  <button type="button" class="btn btn-info disabled btn-md">Even even more click bait</button>
+  <button type="button" class="btn btn-warning disabled btn-md">Even even even more click bait</button>
+  <button type="button" class="btn btn-danger disabled btn-md">Even even even even more click bait</button>
+  <button type="button" class="btn btn-primary disabled btn-md">Even even even even  even more click bait</button>      
+</div>
+  </div>
+</nav>
+<!--end of menue--!>
+
+
+<!-- end of menu if is the iframe-->
+<!-- do not change the following until you have met localhost checkoff #2 -->
+
+
+
+			<div class="embed-responsive embed-responsive-16by9">
+				<iframe  class="embed-responsive-item" name="if" src="intro.html"  ></iframe>
+			</div>
+	</div>
+
+    </div>
+   
+</div>
+
+</body>
+</html>
